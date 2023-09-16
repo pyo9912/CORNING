486c849823031a5e95286ff09f165ff524baaa11
hj <hjhj>
2023-09-16 12:28:05 +0900

    ESPRESSO

--- data_model_know.py
+++ data_model_know.py
@@ -10,9 +10,6 @@ from copy import deepcopy
 
 def truncationPadding(input_ids, max_length, prefix=[], suffix=[]):
     truncate_size = max_length - len(prefix) - len(suffix)
-    # input_ids = prefix + input_ids[-truncate_size:] + suffix
-    # input_ids = input_ids + [0] * (max_length - len(input_ids))
-    # return input_ids
     if truncate_size <= len(input_ids):
         input_ids = prefix + input_ids[len(input_ids) - truncate_size:] + suffix
     else:
@@ -52,8 +49,6 @@ class KnowledgeDataset(Dataset):
 
 
 class DialogDataset(Dataset):
-    """ Retrieve task를 학습하기위한 dialog dataset --> Question (query)
-    """
 
     def __init__(self, args, data_sample, knowledgeDB, train_knowledgeDB, tokenizer, task, mode='train'):
         super(Dataset, self).__init__()
@@ -66,10 +61,6 @@ class DialogDataset(Dataset):
         self.mode = mode
 
     def negative_sampler(self, target_knowledge, candidate_knowledges):
-        # candidate_entity = self.knowledgeDB[target_knowledge][0]
-        # candiate_all_list = self.knowledgeDB_entity_values[candidate_entity]
-        # negative_indice = random.choices(candiate_all_list, k=self.args.negative_num if len(candiate_all_list) > self.args.negative_num else len(candiate_all_list))
-        # total_knowledge_num = self.args.knowledge_num
 
         negative_indice = []
         if len(candidate_knowledges) < self.args.negative_num:
@@ -77,7 +68,6 @@ class DialogDataset(Dataset):
                 negative_indice.append(0)
 
         while len(negative_indice) < self.args.negative_num:
-            # negative_idx = random.randint(0, total_knowledge_num - 1)
             negative_idx = random.choice(candidate_knowledges)
             if (negative_idx not in negative_indice) and (negative_idx not in target_knowledge):
                 negative_indice.append(negative_idx)
@@ -108,24 +98,17 @@ class DialogDataset(Dataset):
         predicted_goal = data['predicted_goal'][0]
 
         if self.mode == 'train':
-            # predicted_goal, predicted_topic = goal, topic
             random.shuffle(predicted_topic_list)
-            # predicted_topic_list = predicted_topic_list[:random.randint(1, self.args.topk_topic)]
             candidate_topic_entities = predicted_topic_list
             predicted_topic = '|'.join(predicted_topic_list)
         else:
             cum_prob = 0
             candidate_topic_entities = []
-            for topic, conf in zip(predicted_topic_list, predicted_topic_confidence_list):
-                candidate_topic_entities.append(topic)
-                cum_prob += conf
-                if cum_prob > self.args.topic_conf:
-                    break
-
-            # predicted_topic_list = [topic for (topic, conf) in zip(predicted_topic_list, predicted_topic_confidence_list) if conf > self.args.topic_conf]
-            # if data['predicted_topic_confidence'][0] > (1 - self.args.topic_conf):
-            #     predicted_topic = data['predicted_topic'][0]
-            # else:
+            for p_topic, conf in zip(predicted_topic_list, predicted_topic_confidence_list):
+                if cum_prob < self.args.topic_conf: # or cum_prob == 0:
+                    candidate_topic_entities.append(p_topic)
+                    cum_prob += conf
+                    # break
             predicted_topic = '|'.join(candidate_topic_entities)
         topic_len = len(candidate_topic_entities)
 
@@ -166,73 +149,10 @@ class DialogDataset(Dataset):
         context_batch['topic_idx'] = self.args.topicDic['str'][topic]  # index로 바꿈
         context_batch['topic'] = self.tokenizer(topic, truncation=True, padding='max_length', max_length=32).input_ids
 
-        # List-wise
-        # if target_knowledge_idx in candidate_knowledges:
-        #     index = candidate_knowledges.index(target_knowledge_idx)
-        #     candidate_knowledges = candidate_knowledges[:index] + candidate_knowledges[index+1:]
-        #     candidate_confidences = candidate_confidences[:index] + candidate_confidences[index + 1:]
-        # candidate_knowledges = [target_knowledge_idx] + candidate_knowledges
-        # candidate_confidences = [100] + candidate_confidences
-
-        # if target_knowledge_idx in candidate_knowledges:
-        #     idxinlist = candidate_knowledges.index(target_knowledge_idx)
-        #     candidate_knowledges.pop(idxinlist)
-        #     candidate_knowledges.insert(0, target_knowledge_idx)
-        #     target_confidence = candidate_confidences.pop(idxinlist)
-        #     candidate_confidences.insert(0, target_confidence)
         candidate_confidences_pos = candidate_confidences[:self.args.pseudo_pos_num]
         candidate_knowledges_pos = candidate_knowledges[:self.args.pseudo_pos_num]
-        # sampling_results = []
-        # for i, data in enumerate(candidate_knowledges_pos):
-        #     if i == 0:
-        #         sampling_results.append(1)
-        #     else:
-        #         sampling_result = np.random.binomial(1, candidate_confidences_pos[i])
-        #         sampling_results.append(sampling_result)
-
-        # random_idx = random.randrange(min(self.args.pseudo_pos_num, len(candidate_knowledges)))
-
-        ### Positive sampling (Top-K)
-        # candidate_knowledges = candidate_knowledges[:self.args.pseudo_pos_num]
-        # candidate_confidences = candidate_confidences[:self.args.pseudo_pos_num]
-        # random_idx = random.randrange(self.args.pseudo_pos_num)
-        # candidate_knowledges = [candidate_knowledges[random_idx]]
-        # candidate_confidences = [candidate_confidences[random_idx]]
-
-        ### Positive sampling (pseudo)
-        # pseudo_candidate_knowledges = [know for know, conf in zip(candidate_knowledges, candidate_confidences) if conf > 0.1]
-        # if len(pseudo_candidate_knowledges) == 0:
-        #     candidate_knowledges = [candidate_knowledges[0]]
-        #     candidate_confidences = [candidate_confidences[0]]
-        # else:
-        #     random_idx = random.randrange(len(pseudo_candidate_knowledges))
-        #     candidate_knowledges = [pseudo_candidate_knowledges[random_idx]]
-        #     candidate_confidences = [pseudo_candidate_knowledges[random_idx]]
-
-        # # Only Top-K
-        # candidate_knowledges = [candidate_knowledges[self.args.pseudo_pos_num]]
-        # candidate_confidences = [candidate_confidences[self.args.pseudo_pos_num]]
-
-        # candidate_knowledges = candidate_knowledges + [0] * (self.args.pseudo_pos_num - len(candidate_knowledges))
-        # candidate_confidences = candidate_confidences + [0] * (self.args.pseudo_pos_num - len(candidate_confidences))
 
         pseudo_negative = self.negative_sampler(candidate_knowledges_pos, candidate_knowledges)
-        # pseudo_negative = [] # todo: remove for rarank!!!!!!!!!!
-
-        ### Grouping
-        # group_num = min(self.args.pseudo_pos_rank, len(candidate_knowledges)) - 1
-        # random_idx = sorted(random.sample(list(range(1, len(candidate_knowledges))), k=group_num))
-        # candidate_knowledges_pos = [candidate_knowledges[0]] + [candidate_knowledges[idx] for idx in random_idx]
-        # # candidate_confidences = [candidate_confidences[0]] + [candidate_confidences[idx] for idx in random_idx]
-        # # candidate_confidences = (np.array(candidate_confidences) - min(candidate_confidences)) / (max(candidate_confidences) - min(candidate_confidences) + 1e-10)
-        # candidate_confidences_pos = list(np.cumprod([0.8] * len(candidate_knowledges_pos)) / 0.8)
-
-        # sampled_pair = sorted(random.sample(list(range(len(candidate_positives_idx))), k=2))
-        # pseudo_positive = candidate_positives_idx[sampled_pair[0]]
-        # pseudo_negative = candidate_positives_idx[sampled_pair[1]]
-
-        # pseudo_positive = random.choice(candidate_positives_idx)
-        # pseudo_positive = candidate_positives_idx[self.args.pseudo_pos_rank - 1]
 
         if self.args.know_ablation == 'target':
             if target_knowledge_idx in candidate_knowledges_pos:
@@ -253,7 +173,6 @@ class DialogDataset(Dataset):
         context_batch['candidate_knowledge_mask'] = candidate_knowledge_mask
 
         context_batch['pseudo_targets'] = candidate_knowledges_pos  # [candidate_knowledges[0]]
-        # context_batch['pseudo_confidences'] = (candidate_confidences_pos > self.args.know_conf)  # + [-1e10] * (self.args.knowledge_num - len(candidate_confidences_pos))
 
         context_batch['target_knowledge'] = [target_knowledge_idx]  # candidate_knowledges[:3]  # target_knowledge_idx
         context_batch['all_negative'] = candidate_knowledges + self.all_negative(candidate_knowledges)
@@ -261,13 +180,12 @@ class DialogDataset(Dataset):
         context_batch['new_knowledge'] = self.knowledgeDB[target_knowledge_idx] not in self.train_knowledgeDB
         context_batch['isFood'] = (goal == 'Food recommendation')
         context_batch['topic_len'] = topic_len
-        # context_batch['sampling_results'] = sampling_results
+        context_batch['candidate_topic_entities'] = [self.args.topicDic['str'][i] for i in candidate_topic_entities]
 
         context_batch['indices'] = idx
         for k, v in context_batch.items():
             if not isinstance(v, torch.Tensor):
                 context_batch[k] = torch.as_tensor(v, device=self.args.device)
-                # context_batch[k] = torch.as_tensor(v)
         return context_batch
 
     def __len__(self):
