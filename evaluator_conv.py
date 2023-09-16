6e0c1389c6b5d5acdc71f4ddba1d1c6a8997fcde
hj <hjhj>
2023-09-16 12:30:07 +0900

    ESPRESSO

--- evaluator_conv.py
+++ evaluator_conv.py
@@ -3,6 +3,7 @@ from nltk import ngrams
 from nltk.translate.bleu_score import sentence_bleu
 from collections import defaultdict
 
+
 class ConvEvaluator:
     def __init__(self, tokenizer, log_file_path):
         self.tokenizer = tokenizer
@@ -21,7 +22,7 @@ class ConvEvaluator:
 
         if log and hasattr(self, 'log_file'):
             for pred, label in zip(decoded_preds, decoded_labels):
-                self.log_file.write(json.dumps({ 'pred': pred, 'label': label }, ensure_ascii=False) + '\n')
+                self.log_file.write(json.dumps({'pred': pred, 'label': label}, ensure_ascii=False) + '\n')
 
         self.collect_ngram(decoded_preds)
         self.compute_bleu(decoded_preds, decoded_labels)
@@ -46,7 +47,8 @@ class ConvEvaluator:
     def report(self):
         report = {}
         for k, v in self.metric.items():
-            if self.sent_cnt == 0: report[k] = 0
+            if self.sent_cnt == 0:
+                report[k] = 0
             else:
                 if 'dist' in k: v = len(v)
                 report[k] = v / self.sent_cnt
@@ -54,15 +56,16 @@ class ConvEvaluator:
         return report
 
     def reset_metric(self):
-        self.metric = { 'bleu@1': 0, 'bleu@2': 0, 'bleu@3': 0, 'bleu@4': 0,
-                        'dist@1': set(), 'dist@2': set(), 'dist@3': set(), 'dist@4': set(), }
+        self.metric = {'bleu@1': 0, 'bleu@2': 0, 'bleu@3': 0, 'bleu@4': 0,
+                       'dist@1': set(), 'dist@2': set(), 'dist@3': set(), 'dist@4': set(), }
         self.sent_cnt = 0
-    
+
     def after_eval_report(self, preds, labels):
         self.collect_ngram(preds)
         self.compute_bleu(preds, labels)
         self.sent_cnt += len([pred for pred in preds if len(pred) > 0])
         pass
+
 
 class ConvEvaluator_ByType:
     def __init__(self, tokenizer, log_file_path):
@@ -82,14 +85,14 @@ class ConvEvaluator_ByType:
 
         if log and hasattr(self, 'log_file'):
             for pred, label, type in zip(decoded_preds, decoded_labels, types):
-                self.log_file.write(json.dumps({ 'pred': pred, 'label': label, 'type':type }, ensure_ascii=False) + '\n')
+                self.log_file.write(json.dumps({'pred': pred, 'label': label, 'type': type}, ensure_ascii=False) + '\n')
 
         self.collect_ngram(decoded_preds, types)
         self.compute_bleu(decoded_preds, decoded_labels, types)
         self.sent_cnt += len([pred for pred in decoded_preds if len(pred) > 0])
-        
+
         for pred, type in zip(preds, types):
-            self.sent_cnt_byType[type] += 1 if len(pred)>0 else 0 
+            self.sent_cnt_byType[type] += 1 if len(pred) > 0 else 0
 
     def collect_ngram(self, strs, types):
         for str, type in zip(strs, types):
@@ -113,19 +116,21 @@ class ConvEvaluator_ByType:
     def report(self):
         report = {}
         for k, v in self.metric.items():
-            if self.sent_cnt == 0: report[k] = 0
+            if self.sent_cnt == 0:
+                report[k] = 0
             else:
                 if 'dist' in k: v = len(v)
                 report[k] = v / self.sent_cnt
         report['sent_cnt'] = self.sent_cnt
         return report
-    
+
     def report_ByType(self):
         report = defaultdict(defaultdict)
-        for type,type_report in self.metric_byType.items():
+        for type, type_report in self.metric_byType.items():
             if isinstance(type, str):
-                for k,v in self.metric_byType[type].items():
-                    if self.sent_cnt_byType[type]==0: report[type][k] = 0
+                for k, v in self.metric_byType[type].items():
+                    if self.sent_cnt_byType[type] == 0:
+                        report[type][k] = 0
                     else:
                         if 'dist' in k: v = len(v)
                         report[type][k] = v / self.sent_cnt_byType[type]
@@ -133,22 +138,23 @@ class ConvEvaluator_ByType:
         return report
 
     def reset_metric(self):
-        self.metric_byType=defaultdict(defaultdict)
-        self.sent_cnt_byType=defaultdict(int)
+        self.metric_byType = defaultdict(defaultdict)
+        self.sent_cnt_byType = defaultdict(int)
 
-        self.metric = { 'bleu@1': 0, 'bleu@2': 0, 'bleu@3': 0, 'bleu@4': 0, 'dist@1': set(), 'dist@2': set(), 'dist@3': set(), 'dist@4': set(), }
+        self.metric = {'bleu@1': 0, 'bleu@2': 0, 'bleu@3': 0, 'bleu@4': 0, 'dist@1': set(), 'dist@2': set(), 'dist@3': set(), 'dist@4': set(), }
         self.sent_cnt = 0
-    
+
     def after_eval_report(self, preds, labels):
         self.collect_ngram(preds)
         self.compute_bleu(preds, labels)
         self.sent_cnt += len([pred for pred in preds if len(pred) > 0])
         pass
 
+
 def conv_gen_eval():
     evaluator = ConvEvaluator(None, None)
     rep_path = "/home/work/CRSTEST/KERS_HJ/epoch_output/2/2023-07-23_052257_BKERS_3711Train_3711Test_1e-5_facebook_bart-base/12_test_GEN_REPORT.txt"
-    preds, labels = [],[]
+    preds, labels = [], []
     with open(rep_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
         for line in lines:
@@ -158,10 +164,53 @@ def conv_gen_eval():
     evaluator.after_eval_report(preds, labels)
     report = evaluator.report()
     report_text = [f"bleu@1, bleu@2, bleu@3, bleu@4, dist@1, dist@2, dist@3, dist@4",
-                  f"{report['bleu@1']:.3f},  {report['bleu@2']:.3f},  {report['bleu@3']:.3f},  {report['bleu@4']:.3f},  {report['dist@1']:.3f},  {report['dist@2']:.3f},  {report['dist@3']:.3f},  {report['dist@4']:.3f}"]
-    print(report_text[0],'\n',report_text[1])
+                   f"{report['bleu@1']:.3f},  {report['bleu@2']:.3f},  {report['bleu@3']:.3f},  {report['bleu@4']:.3f},  {report['dist@1']:.3f},  {report['dist@2']:.3f},  {report['dist@3']:.3f},  {report['dist@4']:.3f}"]
+    print(report_text[0], '\n', report_text[1])
 
 
-if __name__=='__main__':
+# def gen_resp_topic(args, real_resps=None, types=None, topics=None, gen_resps=None):
+#     typelist=['Q&A', 'Movie recommendation', 'Music recommendation', 'POI recommendation', 'Food recommendation'] if args.version !='ko' else ['QA','Movie Recommendation']
+#     hitdic = {type: {'hit1_in_gold': 0, 'hit1_all':0, 'in_gold_cnt':0, 'total': 0} for type in typelist + ['Others', 'total']}
+#     for idx in range(len(real_resps)):
+#         goal_type = types[idx]
+#         if goal_type in typelist: tmp_goal = goal_type
+#         else: tmp_goal = 'Others'
+
+#         pred, gold, topic = gen_resps[idx], real_resps[idx], topics[idx]
+
+#         hitdic['total']['total'] += 1
+#         hitdic[tmp_goal]['total'] += 1
+#         if topic in gold and topic in pred:
+#             hitdic[tmp_goal]['hit1_all'] +=1
+#             hitdic[tmp_goal]['hit1_in_gold'] +=1
+#             hitdic[tmp_goal]['in_gold_cnt'] +=1
+#             hitdic['total']['hit1_all'] +=1
+#             hitdic['total']['hit1_in_gold'] +=1
+#             hitdic['total']['in_gold_cnt'] +=1
+
+#         elif topic in gold:
+#             hitdic[tmp_goal]['in_gold_cnt'] +=1
+#             hitdic['total']['in_gold_cnt'] +=1
+
+#         elif topic in pred:
+#             hitdic[tmp_goal]['hit1_all'] +=1
+#             hitdic['total']['hit1_all'] +=1
+
+#     hitdic_ratio = {goal_type: {'hit1_in_gold': 0, 'hit1_all': 0, 'in_gold_cnt':0, 'total': 0} for goal_type in typelist + ["Others", 'total']}
+#     output_str = [f"                         hit1_in_gold,  hit1_all,  in_gold_cnt, total_cnt"]
+#     for key in hitdic.keys():
+#         if hitdic[key]['total']:
+#             hitdic_ratio[key]['hit1_all'] = hitdic[key]['hit1_all'] / hitdic[key]['total']
+#         if hitdic[key]['in_gold_cnt']:
+#             hitdic_ratio[key]['hit1_in_gold'] = hitdic[key]['hit1_in_gold'] / hitdic[key]['in_gold_cnt']
+
+#         hitdic_ratio[key]['total'] = hitdic[key]['total']
+#         hitdic_ratio[key]['in_gold_cnt'] = hitdic[key]['in_gold_cnt']
+#         output_str.append(f"{key:^22}: {hitdic_ratio[key]['hit1_in_gold']:.3f}, {hitdic_ratio[key]['hit1_all']:.3f}, {hitdic_ratio[key]['in_gold_cnt']}, {hitdic_ratio[key]['total']}")
+#     return hitdic, hitdic_ratio, output_str
+
+
+if __name__ == '__main__':
     import json
+
     conv_gen_eval()
